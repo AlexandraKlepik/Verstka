7f9a2a947707f12dbacc02996a1a64bdf2fd95be
datcukbogdan <datcukbogdan@gmail.com>
2021-09-17 22:11:19 +0300

    hw

---
+++ teach/4/js/script.js
@@ -0,0 +1,12 @@
+var x = 0, y = 5, summ;
+
+if(x == 4 || x > 10){
+  console.log("x більше 0");
+}
+else{
+  console.log("x не дорівнює три");
+}
+
+for (var i = 10; i > 0; i--) {
+    console.log(i);
+}
